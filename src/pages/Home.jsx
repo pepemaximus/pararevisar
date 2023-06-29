c5fd305f741aeed90f388fb4ea89816bcc0eb3b2
pepemaximus <103108647+pepemaximus@users.noreply.github.com>
2023-06-29 10:40:38 -0300

    Add files via upload

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,37 +1,28 @@
 import React, { useEffect, useState } from "react";
 import ItemListContainer from "../components/ItemListContainer/ItemListContainer";
 import { getProducts } from "../mock";
-import { collection, getDocs, getDoc, getFirestore } from "firebase/firestore";
+import { collection, getDocs, getFirestore } from "firebase/firestore";
 
 const Home = () => {
 
-    const [products, setProducts] = useState([])    
-    const [error, setError] = useState(false);
-    const [typeError, setTypeError] = useState ("");
+    const [products, setProducts] = useState([]);
+    const [error,setError] = useState(false);
+    const [typeError, setTypeError] = useState("");    
 
-    useEffect (() => {
+    useEffect(() => { 
         const db = getFirestore();
-        const itemListCollection = collection (db, "itemList");
-        getDocs(itemListCollection)
-        .then((snapshot) => {
-            setProducts (snapshot.docs.map((doc) => ({id: doc.id, ...doc.data()})));
-        } )
-        .catch((error) => setError(true));
-    }
-    )
-
-
-
-
-    if(products.length > 0) {
-
+        const productCollection = collection (db,"itemList");
+        getDocs(productCollection).then((snapshot) => {
+            setProducts(snapshot.docs.map((doc) => ({id: doc.id, ...doc.data()})));
+        })
+        .catch((error) => setError(true))
+        console.log(products);
         return (
             <div>
                 <ItemListContainer greeting={'Welcome to Tech Store'} products={products}/>
             </div>
     )
+})
 }
-}
-
 export default Home;
 
